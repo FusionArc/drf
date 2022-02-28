16c4f6c50fde6ae501e32637f51cf115e2bfdf32
David Barrow <davecheez21@gmail.com>
2022-02-28 21:30:07 +0000

    update project

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,10 +1,12 @@
 import requests
 
 endpoint = "https://httpbin.org/anything"
+backend = "http://localhost:8000"
 
-response = requests.get(endpoint, json={"query":"Hello World!"})
+# response = requests.get(backend, json={"query":"Hello World!"})
+response = requests.get(backend)
 source = response.text
-json_response = response.json()
 status = response.status_code
+# json_response = response.json()
 
-print(json_response)
+print(source)
